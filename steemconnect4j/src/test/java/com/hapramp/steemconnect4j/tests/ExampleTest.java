edaa5e49257297d310e0d2bcdf2ad482f63fbb8f
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-06-14 10:47:55 +0530

    Added tool version to jacoco

---
+++ steemconnect4j/src/test/java/com/hapramp/steemconnect4j/tests/ExampleTest.java
@@ -0,0 +1,10 @@
+package com.hapramp.steemconnect4j.tests;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void testMe(){
+        System.out.print("Hello");
+    }
+}
