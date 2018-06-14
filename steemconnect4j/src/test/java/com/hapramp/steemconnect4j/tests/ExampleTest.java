a8e33e172394fd7770ac53acffadccabc0480575
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-06-14 11:32:59 +0530

    Added toolversion to jacoco

--- steemconnect4j/src/test/java/com/hapramp/steemconnect4j/tests/ExampleTest.java
+++ steemconnect4j/src/test/java/com/hapramp/steemconnect4j/tests/ExampleTest.java
@@ -1,10 +1,19 @@
 package com.hapramp.steemconnect4j.tests;
 
+import com.hapramp.steemconnect4j.SteemConnect;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
     @Test
-    public void testMe(){
-        System.out.print("Hello");
+    public void testMe() {
+        assertTrue(SteemConnect.getLoginUrl().length() > 0);
+    }
+
+    @Test
+    public void testSteemConnect() {
+
     }
 }
