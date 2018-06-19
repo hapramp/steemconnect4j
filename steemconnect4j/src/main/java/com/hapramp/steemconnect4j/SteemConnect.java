153754be626c1a083c30f2c4c17f27698a646073
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-06-19 14:09:04 +0530

    add ignore method

--- steemconnect4j/src/main/java/com/hapramp/steemconnect4j/SteemConnect.java
+++ steemconnect4j/src/main/java/com/hapramp/steemconnect4j/SteemConnect.java
@@ -179,6 +179,25 @@ public class SteemConnect {
     this.broadcast(operations,steemConnectCallback);
   }
 
+  /**
+  * ignore a user.
+  * @param follower               follower
+  * @param following              user to be followed
+  * @param steemConnectCallback   callback for response
+  */
+  public void ignore(String follower, String following, SteemConnectCallback steemConnectCallback) {
+    String params = StringUtils.getCommanSeparatedObjectString(
+        RpcJsonUtil.getKeyValuePair("required_auths","[]"),
+        RpcJsonUtil.getKeyValuePair("required_posting_auths","[" + follower + "]"),
+        RpcJsonUtil.getKeyValuePair("id","follow"),
+        RpcJsonUtil.getKeyValuePair("json", StringUtils.getCommanSeparatedArrayString("follow",
+            StringUtils.getCommanSeparatedObjectString(follower,following,
+            RpcJsonUtil.getKeyValuePair("what","[ignore]"))))
+    );
+
+    String operations = StringUtils.getCommanSeparatedArrayString("custom_json",params);
+    this.broadcast(operations,steemConnectCallback);
+  }
 
   public static class InstanceBuilder {
     SteemConnectOptions steemConnectOptions;
