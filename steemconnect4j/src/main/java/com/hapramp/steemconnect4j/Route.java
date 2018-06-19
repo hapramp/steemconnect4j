1a23314d50425ecfb7a65d1a4c4f18b12193c994
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-06-19 14:41:38 +0530

    add revoke token  method

--- steemconnect4j/src/main/java/com/hapramp/steemconnect4j/Route.java
+++ steemconnect4j/src/main/java/com/hapramp/steemconnect4j/Route.java
@@ -3,4 +3,5 @@ package com.hapramp.steemconnect4j;
 public class Route {
   public static final String BROADCAST = "broadcast";
   public static final String ME = "me";
+  public static final String REVOKE_TOKEN = "oauth2/token/revoke";
 }
